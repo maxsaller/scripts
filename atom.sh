30d9afb08878ac98772873769175da02d58f4c2c
Max Saller <36445767+maxsaller@users.noreply.github.com>
2018-10-17 10:57:44 +0200

    atom.sh for quick re-install of atom

---
+++ atom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+apm install atom-ide-ui ide-python language-fortran ide-fortran tree-view-git-status tree-view-sort git-plus language-latex latex-autocomplete autocomplete-latex latex-wordcount pdf-view busy-signal build build-make script platformio-ide-terminal
+
+echo '
+PLATFORMIO-IDE-TERMINAL: Auto Run Command: export PS1="[\w]\$ ";clear'
