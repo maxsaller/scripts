d377cfa7fa282fe88a2ead4ec8e48a7a8187ad91
Max Saller <36445767+maxsaller@users.noreply.github.com>
2018-10-17 15:22:24 +0200

    Update atom.sh

--- atom.sh
+++ atom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apm install atom-ide-ui ide-python language-fortran ide-fortran tree-view-git-status tree-view-sort file-icons git-plus language-latex latex-autocomplete autocomplete-latex latex-wordcount pdf-view busy-signal build build-make script platformio-ide-terminal
+apm install atom-ide-ui ide-python language-fortran ide-fortran tree-view-git-status tree-view-sort file-icons git-plus language-latex latex-autocomplete autocomplete-latex latex-wordcount pdf-view busy-signal build build-make script platformio-ide-terminal minimap minimap-cursorline minimap-hide
 
 echo '
 PLATFORMIO-IDE-TERMINAL: Auto Run Command: export PS1="[\w]\$ ";clear'
